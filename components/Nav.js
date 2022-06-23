2b3cb78b9ed996551f479381309feddc7a49c9b8
jklagrosa <jklagrosa.dev@gmail.com>
2022-06-23 18:18:50 +0800

    Re-deployed

--- components/Nav.js
+++ components/Nav.js
@@ -1,41 +1,12 @@
-import { useState } from "react";
-import { Modal } from "antd";
-import VideoModal from "./VideoModal";
-
 const Navigation = () => {
-  const [isModalVisible, setIsModalVisible] = useState(false);
-  const handleCancel = () => {
-    setIsModalVisible(false);
-    console.log("closed");
-  };
-
-  const showModal = () => {
-    setIsModalVisible(true);
-  };
-
   return (
     <>
-      Pariatur sint incididunt nostrud duis. Labore mollit nisi ipsum anim
-      fugiat dolore mollit. Labore ex tempor tempor pariatur dolor consectetur
-      do quis sit.
-      <iframe
-        width="600"
-        height="500"
-        src={`https://www.youtube.com/embed/R_UJYQoWVwY?autoplay=1`}
-        title="YouTube video player"
-        frameborder="0"
-        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-        allowfullscreen
-      ></iframe>
-      <Modal
-        title="Basic Modal"
-        visible={isModalVisible}
-        onCancel={handleCancel}
-        width={1000}
-      >
-        <VideoModal url={""} />
-      </Modal>
-      <button onClick={showModal}>Play</button>
+      Minim laboris magna ullamco officia aute minim exercitation ex proident
+      officia. Minim dolore consequat magna sunt excepteur eiusmod ad nisi. Ut
+      nulla ipsum Lorem tempor qui voluptate cillum magna magna ad.
+      Reprehenderit eiusmod nostrud esse esse dolor ipsum ullamco eu
+      exercitation fugiat est voluptate. Elit do ad ipsum voluptate nostrud ex
+      deserunt nulla officia. Veniam amet veniam do non.
     </>
   );
 };
