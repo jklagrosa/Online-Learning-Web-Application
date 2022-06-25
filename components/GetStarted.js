8d559b87784a39a6e5a0043fec17d65ef3794cb6
jklagrosa <jklagrosa.dev@gmail.com>
2022-06-25 21:36:04 +0800

    Added imgs and text to GS component

--- components/GetStarted.js
+++ components/GetStarted.js
@@ -6,11 +6,31 @@ const GetStarted = () => {
     <>
       <div id={styles._gs_wrapper}>
         <Container fluid="lg" className="p-0">
-          <Row className="gx-3 gy-5">
-            <Col xs={6} md={3}>
-                <div className={styles._gs_boxes}>
-          
-                </div>
+          <h1>Explore and Complete a Guided Project</h1>
+
+          <Row className="gx-3 gy-4">
+            <Col xs={12} sm={6} md={4}>
+              <div className={styles._gs_boxes}>
+                <img src="/gs/1.png" />
+                <h3>Introduction to Web Development</h3>
+                <h4>Longina Phil</h4>
+              </div>
+            </Col>
+            {/* ======= */}
+            <Col xs={12} sm={6} md={4}>
+              <div className={styles._gs_boxes}>
+                <img src="/gs/2.png" />
+                <h3>Introduction to Graphic Design</h3>
+                <h4>Aleid Dagda</h4>
+              </div>
+            </Col>
+            {/* ======= */}
+            <Col xs={12} sm={6} md={4}>
+              <div className={styles._gs_boxes}>
+                <img src="/gs/3.jpg" />
+                <h3>Introduction to Programming</h3>
+                <h4>David Sopas</h4>
+              </div>
             </Col>
           </Row>
         </Container>
