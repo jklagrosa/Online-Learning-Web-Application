350a219bee147017938770f6753028eeeb369837
jklagrosa <jklagrosa.dev@gmail.com>
2022-06-27 16:50:48 +0800

    Added row, col to footer component

--- components/Footer.js
+++ components/Footer.js
@@ -1,11 +1,18 @@
+import styles from "../styles/Footer.module.scss";
+import { Container, Row, Col } from "react-bootstrap";
+
 const Footer = () => {
   return (
     <>
-      Officia ex ullamco amet ex culpa nulla labore ex nostrud magna Lorem sunt.
-      Enim mollit culpa nulla non ad adipisicing minim exercitation in non non
-      adipisicing. Ut et incididunt incididunt laboris ex ad irure reprehenderit
-      duis qui deserunt ipsum minim sunt. Sint ut pariatur sint proident aliquip
-      ut culpa do mollit dolor velit. Ut sit eu consectetur sunt et tempor.
+      <div id={styles._footer_wrapper}>
+        <Container fluid="lg" className="p-0">
+          <Row className="gx-3 gy-4">
+            <Col>
+                
+            </Col>
+          </Row>
+        </Container>
+      </div>
     </>
   );
 };
