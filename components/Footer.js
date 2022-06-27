6b869a5a45607908fdc00fca996607e8bfedcdae
jklagrosa <jklagrosa.dev@gmail.com>
2022-06-27 16:42:34 +0800

    Added footer component

---
+++ components/Footer.js
@@ -0,0 +1,13 @@
+const Footer = () => {
+  return (
+    <>
+      Officia ex ullamco amet ex culpa nulla labore ex nostrud magna Lorem sunt.
+      Enim mollit culpa nulla non ad adipisicing minim exercitation in non non
+      adipisicing. Ut et incididunt incididunt laboris ex ad irure reprehenderit
+      duis qui deserunt ipsum minim sunt. Sint ut pariatur sint proident aliquip
+      ut culpa do mollit dolor velit. Ut sit eu consectetur sunt et tempor.
+    </>
+  );
+};
+
+export default Footer;
