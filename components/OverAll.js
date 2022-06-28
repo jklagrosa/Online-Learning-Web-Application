c99a46d47ab8b569b53e9833ac9e4102f1b952d3
jklagrosa <jklagrosa.dev@gmail.com>
2022-06-28 17:46:40 +0800

    Added media queries to overall comp upto 2560px

--- components/OverAll.js
+++ components/OverAll.js
@@ -6,13 +6,42 @@ const OverAll = () => {
     <>
       <div id={styles._overall_wrapper}>
         <Container fluid="lg" className="p-0">
+          {/* <h1>Strength in Numbers</h1> */}
           <Row className="gx-3 gy-4">
-            <Col xs={12} md={6} lg={4}>
+            <Col xs={6} sm={6} lg={3}>
+              <div
+                className={styles._overall_details}
+                id={styles._overall_details_ID}
+              >
+                <h3>40K+</h3>
+                <h6>Learners &amp; counting</h6>
+              </div>
+            </Col>
+            {/* === */}
+            <Col xs={6} sm={6} lg={3}>
+              <div
+                className={styles._overall_details}
+                id={styles._overall_details_ID}
+              >
+                <h3>153+</h3>
+                <h6>Total courses</h6>
+              </div>
+            </Col>
+            {/* === */}
+            <Col xs={6} sm={6} lg={3}>
+              <div
+                className={styles._overall_details}
+                id={styles._overall_details_ID}
+              >
+                <h3>54+</h3>
+                <h6>Total instructors</h6>
+              </div>
+            </Col>
+            {/* === */}
+            <Col xs={6} sm={6} lg={3}>
               <div className={styles._overall_details}>
-                Aliqua magna est commodo ullamco id. Esse dolore reprehenderit
-                aliqua aliquip reprehenderit est proident excepteur quis fugiat
-                exercitation dolor. Et eu enim deserunt occaecat eiusmod
-                pariatur veniam amet non adipisicing cupidatat.
+                <h3>93%</h3>
+                <h6>Successful students</h6>
               </div>
             </Col>
           </Row>
