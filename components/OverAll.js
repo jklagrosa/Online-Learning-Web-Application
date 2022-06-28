2e5c8a17be361ca9ce49be4e7f337e23f46d6e74
jklagrosa <jklagrosa.dev@gmail.com>
2022-06-28 16:28:06 +0800

    Added overall component and module.scss

---
+++ components/OverAll.js
@@ -0,0 +1,18 @@
+import styles from "../styles/OverAll.module.scss";
+import { Container, Row, Col } from "react-bootstrap";
+
+const OverAll = () => {
+  return (
+    <>
+      Qui est laboris officia irure irure culpa ea sint aliqua voluptate quis
+      laborum ad. Excepteur adipisicing cillum velit eu qui ex proident veniam
+      qui in irure eiusmod commodo aute. In incididunt ex ad veniam voluptate
+      laborum. Pariatur consequat sunt elit nulla anim dolor mollit nisi
+      excepteur. Exercitation officia non magna quis aute sunt velit consequat.
+      Pariatur excepteur ut occaecat non. Adipisicing est nulla sunt nostrud
+      duis sunt ipsum voluptate voluptate elit cupidatat.
+    </>
+  );
+};
+
+export default OverAll;
