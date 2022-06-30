35f7dd162c93fda7768bedbdd2c6458abf3bec2e
jklagrosa <jklagrosa.dev@gmail.com>
2022-06-30 16:22:06 +0800

    Added course page

---
+++ pages/course/[id].js
@@ -0,0 +1,13 @@
+const CourseID = () => {
+  return (
+    <>
+      Cupidatat dolore ut Lorem Lorem tempor elit. Sint voluptate et eiusmod
+      duis aute. Qui eu amet Lorem adipisicing ullamco. Consequat excepteur
+      cillum sunt tempor excepteur consequat dolore duis. Culpa minim occaecat
+      et laborum reprehenderit sint sunt commodo. Fugiat velit dolor eiusmod in
+      non ipsum cupidatat nulla excepteur irure ea est elit est.
+    </>
+  );
+};
+
+export default CourseID;
