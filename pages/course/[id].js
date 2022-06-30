245754e04357458adf8e6f5b64dd51d046192bbd
jklagrosa <jklagrosa.dev@gmail.com>
2022-06-30 18:05:50 +0800

    Added row, col in course pages

--- pages/course/[id].js
+++ pages/course/[id].js
@@ -1,8 +1,13 @@
 import styles from "../../styles/COURSE.module.scss";
-import { Container, Row, Col, Breadcrumb } from "react-bootstrap";
+import { Container, Row, Col, Breadcrumb, Badge } from "react-bootstrap";
 import Nav from "../../components/Nav";
 import Footer from "../../components/Footer";
 import Copyright from "../../components/Copyright";
+
+import { AiFillStar, AiFillCaretRight } from "react-icons/ai";
+import { BsPersonFill } from "react-icons/bs";
+import { FaChalkboardTeacher } from "react-icons/fa";
+import { MdPlayLesson } from "react-icons/md";
 
 const CourseID = () => {
   return (
@@ -32,8 +37,37 @@ const CourseID = () => {
                 {/* ================================= */}
 
                 <div className={styles._course_header_details}>
+                  <Badge className={styles._course_tag}>Web Development</Badge>{" "}
                   <h1>Take A Course For Web Development</h1>
+                  <div className={styles._course_contents}>
+                    <span>
+                      <FaChalkboardTeacher
+                        className={styles._course_contents_ICONS}
+                      />{" "}
+                      David Sopas
+                    </span>
+                    {/*  */}
+                    <span>
+                      <AiFillStar className={styles._course_contents_ICONS} />{" "}
+                      4.2/5
+                    </span>
+                    {/*  */}
+                    <span>
+                      <MdPlayLesson className={styles._course_contents_ICONS} />{" "}
+                      3 Lessons
+                    </span>
+                    {/*  */}
+                    <span>
+                      <BsPersonFill className={styles._course_contents_ICONS} />{" "}
+                      25 Enrolled Students
+                    </span>
+                  </div>
                 </div>
+
+
+                {/* COURSE OVERVIEW */}
+
+
               </Col>
             </Row>
           </div>
