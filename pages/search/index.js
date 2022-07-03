3f8ddc4b8755ced29bbf61015175d53fe75a0dd3
jklagrosa <jklagrosa.dev@gmail.com>
2022-07-03 14:17:15 +0800

    Added: search page

---
+++ pages/search/index.js
@@ -0,0 +1,14 @@
+const Search = () => {
+  return (
+    <>
+      Cupidatat nisi id enim nostrud aliqua occaecat deserunt sint.
+      Reprehenderit ut veniam aute proident cillum veniam. Magna laboris aliquip
+      nisi nostrud veniam consectetur Lorem esse voluptate. Aliqua ipsum nulla
+      labore id irure minim consequat exercitation ad commodo duis commodo
+      ipsum. Exercitation voluptate mollit eiusmod ea commodo cillum Lorem
+      laborum quis duis ad aute quis non.
+    </>
+  );
+};
+
+export default Search;
