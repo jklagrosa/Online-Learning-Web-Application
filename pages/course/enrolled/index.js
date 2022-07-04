e6e26a1077a029bba81b7048aba62b3f83d19827
jklagrosa <jklagrosa.dev@gmail.com>
2022-07-04 14:56:58 +0800

    Added: course/enrolled/index page

---
+++ pages/course/enrolled/index.js
@@ -0,0 +1,14 @@
+const EnrolledCourse = () => {
+  return (
+    <>
+      Officia elit reprehenderit reprehenderit occaecat id irure ad aliquip
+      occaecat. Cillum tempor eu adipisicing laborum consectetur. Voluptate enim
+      consequat incididunt amet consequat consequat pariatur ut laboris. Aute
+      nulla reprehenderit mollit non ut nisi aliqua excepteur laboris proident.
+      Tempor deserunt labore non proident cillum fugiat. Exercitation laborum
+      sunt fugiat sit commodo sunt.
+    </>
+  );
+};
+
+export default EnrolledCourse;
