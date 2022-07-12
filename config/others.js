fd83eb42c0095eb76f2f1ca9f241a5376081d15b
jklagrosa <jklagrosa.dev@gmail.com>
2022-07-12 17:16:01 +0800

    Changed the url from development to production

--- config/others.js
+++ config/others.js
@@ -1,6 +1,6 @@
-export const BASE_URL = "http://localhost:3000";
+// export const BASE_URL = "http://localhost:3000";
 
-// export const BASE_URL = "https://online-learning-web-application.vercel.app/";
+export const BASE_URL = "https://online-learning-web-application.vercel.app/";
 
 export const headersOpts = {
   headers: {
